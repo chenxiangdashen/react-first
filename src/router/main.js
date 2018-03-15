d57bfeb43c55015898e998c3e62b298b8c2c8338
apple <xiang.chen@sotos.cn>
2018-03-15 17:29:56 +0800

    aaa

---
+++ src/router/main.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import {Route, Switch, Redirect} from 'react-router-dom'
+import {message} from 'antd'
+import {connect} from 'react-redux'
+import {bindActionCreators} from 'redux'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css' // Progress 进度条样式
